d6adef53945b5f96ee0514c279811d2cb91f6bef
Vladimir Kouprianov <v.kouprianov@gmail.com>
2019-02-04 22:19:03 -0500

    Removed obsolete data-files/#/phot-cal endpoint

--- afterglow_server/resources/__init__.py
+++ afterglow_server/resources/__init__.py
@@ -3,4 +3,4 @@ Afterglow Access Server: package containing all resources provided by the
 server API
 """
 
-from . import data_providers, data_files, jobs, photometry, phot_cal
+from . import data_providers, data_files, jobs, photometry
