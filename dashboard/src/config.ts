2822fd6978e1d0350cb97f48d04cf838e7578b4e
Joshua Haislip <jhaislip@gmail.com>
2021-04-08 17:35:30 -0400

    remove oauthUrl from config

--- dashboard/src/config.ts
+++ dashboard/src/config.ts
@@ -1,6 +1,5 @@
 export const appConfig = {
   coreUrl: "/core",
-  oauthUrl: "/oauth2",
   csrfCookieName: "afterglow_core.csrf",
   csrfHeaderName: "x-afterglow-core-csrf-token",
   authCookieName: "afterglow_core_access_token",
