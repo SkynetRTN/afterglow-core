adbbdb16af9afe1e9c69c665df77c8c5d2587a59
Vladimir Kouprianov <v.kouprianov@gmail.com>
2022-03-24 23:51:21 -0400

    Fixed Stetson catalog RA/Dec

--- afterglow_core/resources/catalog_plugins/stetson_globs_catalog.py
+++ afterglow_core/resources/catalog_plugins/stetson_globs_catalog.py
@@ -13,20 +13,20 @@ class StetsonGlobsCatalog(VizierCatalog):
     """
     Stetson's globular cluster photometry catalog plugin
     """
-    name = 'Photometry of Globular Clusters'
+    name = 'StetsonGlobs'
     display_name = 'UBVRI photometry in 48 globular clusters (Stetson+, 2019)'
     num_sources = 4890955
     vizier_catalog = 'J/MNRAS/485/3042/table4'
     row_limit = 10000
     col_mapping = {
         'id': 'Star',
-        'ra_hours': 'int(RAJ2000.value[0].split()[0]) + '
-        'int(RAJ2000.value[0].split()[1])/60 + '
-        'float(RAJ2000.value[0].split()[2])/3600',
-        'dec_degs': '(abs(int(DEJ2000.value[0].split()[0])) + '
-        'int(DEJ2000.value[0].split()[1])/60 + '
-        'float(DEJ2000.value[0].split()[2])/3600)*'
-        '(1 - 2*(DEJ2000.value[0].split()[0].startswith("-")))',
+        'ra_hours': 'int(RAJ2000.split()[0]) + '
+        'int(RAJ2000.split()[1])/60 + '
+        'float(RAJ2000.split()[2])/3600',
+        'dec_degs': '(abs(int(DEJ2000.split()[0])) + '
+        'int(DEJ2000.split()[1])/60 + '
+        'float(DEJ2000.split()[2])/3600)*'
+        '(1 - 2*(DEJ2000.split()[0].startswith("-")))',
     }
     mags = {
         'U': ('Umag', 'e_Umag'), 'B': ('Bmag', 'e_Bmag'),
