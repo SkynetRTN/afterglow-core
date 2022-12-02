ce60b46a021e0c130c0b93e6f351db8cdd6ca6e8
Vladimir Kouprianov <v.kouprianov@gmail.com>
2022-12-02 05:08:04 -0500

    Simplify dev launcher script

--- scripts/start_afterglow_core.py
+++ scripts/start_afterglow_core.py
@@ -2,12 +2,9 @@
 
 if __name__ in ('__main__', 'start_afterglow_core'):
     # Running in dev environment
+    from afterglow_core.job_server import init_jobs
+    init_jobs()
+
     from afterglow_core import create_app
     app = create_app()
-
-    with app.app_context():
-        from afterglow_core.job_server import init_jobs
-        init_jobs()
-
-    if __name__ == '__main__':
-        app.run(threaded=False, use_reloader=False)
+    app.run(threaded=False, use_reloader=False)
